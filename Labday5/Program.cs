830163c051f0481ae188f249c14793f34b0228b4
Jamila Ismayilzada <jamila.ismayilzada@code.local>
2023-05-05 13:35:00 +0400

    added codes

---
+++ Labday5/Program.cs
@@ -0,0 +1,8 @@
+﻿//Math
+//Console.WriteLine(Math.Abs(-34));//musbet edede cevirir
+//Console.WriteLine(Math.PI);// pi herfidir 
+//Console.WriteLine(Math.Cbrt(26));//3cu dereceden kok  Cube root
+//Console.WriteLine(Math.Sqrt(4));//2-ci dereceden kok  Square root 
+Console.WriteLine(Math.Ceiling(3.9));// ceiling -ne olursa olsun BOYUK EDEDI GOTURUR
+Console.WriteLine(Math.Floor(3.9));// FLOOR -ne olursa olsun kicik EDEDI GOTURUR
+Console.WriteLine(Math.Round(3.4));// round -yuvarlaqlasdirir
